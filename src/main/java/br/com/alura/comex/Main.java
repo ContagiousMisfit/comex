4b8ee076dc7b8387783087c6db57b21f24d84ea9
Isabela <isacaovila@gmail.com>
2022-05-13 11:56:35 -0300

    Alteração nos parametros do output no console

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -20,36 +20,13 @@ public class Main {
     	ProcessadorDeCSV processadorDeCSV = new ProcessadorDeCSV();
     	pedidos = processadorDeCSV.listarPedidos(arquivoCSV);
 
-        int totalDeProdutosVendidos = 0;
-        int totalDePedidosRealizados = 0;
-        BigDecimal montanteDeVendas = BigDecimal.ZERO;
-        Pedido pedidoMaisBarato = null;
-        Pedido pedidoMaisCaro = null;
-
-        CategoriasProcessadas categoriasProcessadas = new CategoriasProcessadas();
-        int totalDeCategorias = 0;
-
-        for (int i = 0; i < pedidos.size(); i++) {
-            Pedido pedidoAtual = pedidos.get(i);
-
-            montanteDeVendas = montanteDeVendas.add(pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())));
-            totalDeProdutosVendidos += pedidoAtual.getQuantidade();
-            totalDePedidosRealizados++;
-
-            if (!categoriasProcessadas.contains(pedidoAtual.getCategoria())) {
-              totalDeCategorias++;
-              categoriasProcessadas.add(pedidoAtual.getCategoria());
-            }
-        }
-        
-
         
         System.out.println("#### RELATÓRIO DE VALORES TOTAIS");
         System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", totalDePedidosRealizados);
         System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", totalDeProdutosVendidos);
         System.out.printf("- TOTAL DE CATEGORIAS: %s\n", totalDeCategorias);
         System.out.printf("- MONTANTE DE VENDAS: %s\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(montanteDeVendas.setScale(2, RoundingMode.HALF_DOWN)));
-        System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(Pedido.getPedidoMaisBarato(pedidos).get().getPreco()), Pedido.getPedidoMaisBarato(pedidos).get().getProduto());
+        System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(Pedido.getPedidoMaisBarato(pedidos).get().getValorTotal()), Pedido.getPedidoMaisBarato(pedidos).get().getProduto());
         System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(Pedido.getPedidoMaisCaro(pedidos).get().getValorTotal()), Pedido.getPedidoMaisCaro(pedidos).get().getProduto());
     }
 }
